403f48d468f23b54a5724a1fe957a8328b345093
RedaLegzali <m.reda.legzali@gmail.com>
2021-03-06 22:21:59 +0100

    Docker Compose

---
+++ README.md
@@ -0,0 +1,16 @@
+# MovieAppInfra
+
+* Description
+* Technologies
+
+## Description
+
+This repository contains the Infrastructure blueprint to deploy the MovieApp application.
+
+## Technologies
+
+* Infra as Code
+	* Vagrant
+* Infra Provisioning
+	* Ansible
+  * Jenkins
---
+++ Vagrant/provisions/dev.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+ssh-keygen -t rsa -b 2048 -f /home/vagrant/.ssh/id_rsa -q -N ''
+chown -R vagrant:vagrant /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+chmod 644 /home/vagrant/.ssh/id_rsa.pub
+chmod 600 /home/vagrant/.ssh/id_rsa /home/vagrant/.ssh/authorized_keys
